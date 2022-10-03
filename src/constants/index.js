37a27c8c4e16af922b741e9b5ce321035c809af6
fidellim <lim.fidel@gmail.com>
2022-10-04 00:12:54 +0400

    add constants

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const products = [
+    {
+        itemName: 'Perfume',
+        productName: 'Gabrielle Essence Eau De Parfum',
+        description:
+            '  A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.',
+        discountPrice: '$149.99',
+        originalPrice: '$169.99',
+    },
+]
