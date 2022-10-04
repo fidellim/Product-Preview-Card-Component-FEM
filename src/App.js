0ed7bf38205aec2713bf7b357f63643ad88c9b1b
fidellim <lim.fidel@gmail.com>
2022-10-04 13:06:23 +0400

    add card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Card from './components/Card'
+import { products } from './constants'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    return (
+        <div className="w-screen min-h-screen bg-cream">
+            <main className="w-screen min-h-screen flex justify-center items-center">
+                {products.map((product) => (
+                    <Card product={product} key={product.productName} />
+                ))}
+            </main>
+        </div>
+    )
 }
 
 export default App
