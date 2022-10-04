a634113025ee93e50273f1fbe1bb6f9b03cfc260
fidellim <lim.fidel@gmail.com>
2022-10-04 13:04:39 +0400

    add colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                darkCyan: '#3c8067',
+                darkerCyan: '#1f4234',
+                cream: '#f2ebe3',
+                darkBlue: '#1c232b',
+                grayishBlue: '#6c7289',
+            },
+            fontFamily: {
+                fraunces: ['Fraunces', 'serif'],
+                montserrat: ['Montserrat', 'sans-serif'],
+            },
+        },
     },
     plugins: [],
 }
